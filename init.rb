752819bb735876173178b2db2cb56089637dff74
Mariusz Pietrzyk <wijet@wijet.pl>
2009-10-15 02:40:37 +0200

    Fixed init.rb, stopped the plugin from crashing when enumeration file doesn't exist

--- init.rb
+++ init.rb
@@ -1,5 +1,7 @@
-require 'simple_enumerations'
-require 'symbol_extension'
-require 'action_view/form_helper'
-ActiveRecord::Base.send(:include, Wijet::SimpleEnumerations)
-ActionView::Helpers::FormBuilder.send(:include, Wijet::FormHelper)
+if File.exists?(File.join(RAILS_ROOT, 'config', 'enumerations.yml'))
+  require 'simple_enumerations'
+  require 'symbol_extension'
+  require 'action_view/form_helper'
+  ActiveRecord::Base.send(:include, Wijet::SimpleEnumerations)
+  ActionView::Helpers::FormBuilder.send(:include, Wijet::FormHelper)
+end
